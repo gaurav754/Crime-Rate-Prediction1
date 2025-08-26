9f59679b9b0f2d02a13a5dfe2132565fe375b15b
divyansh <divyanshkld899@gmail.com>
2025-08-26 11:22:42 +0530

    changes

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
   <div className="flex items-center justify-center h-screen">
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Print hello:
     </h1>
   </div>
 
